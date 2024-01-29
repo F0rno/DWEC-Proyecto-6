44ec74383b4ad48310904008fe08a41075a0ce2f
F0rno <pablo.fornell.perinan@gmail.com>
2024-01-29 10:20:54 +0100

    internal(Routing): First contact with basic routing

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createMemoryHistory } from "vue-router";
+
+import Home from "../views/Home.vue";
+
+const routes = [
+    {
+        path: "/home",
+        name: "Home",
+        component: Home,
+    },
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <h1>Hola home</h1>
+        <RouterLink to="/">App</RouterLink>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
