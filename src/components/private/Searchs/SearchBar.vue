d0f3c330dc878dc1d08b06c702364ca60da3acd7
F0rno <pablo.fornell.perinan@gmail.com>
2024-02-02 13:18:40 +0100

    internal: SearchBar component

---
+++ src/components/private/Searchs/SearchBar.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+    props: {
+        defaultInput: {
+            type: String,
+            default: ''
+        },
+        defaultType: {
+            type: String,
+            default: 'book'
+        },
+        defaultSortedBy: {
+            type: String,
+            default: 'new'
+        },
+        onSubmit: {
+            type: Function,
+            default: () => {}
+        }
+    },
+    data() {
+        return {
+            input: this.defaultInput,
+            type: this.defaultType,
+            sortedBy: this.defaultSortedBy
+        }
+    },
+    methods: {
+        onChange(event) {            
+            if (event.target.name === 'type') {
+                this.type = event.target.value
+            } else if (event.target.name === 'sortedby') {
+                this.sortedBy = event.target.value
+            } else {
+                this.input = event.target.value
+            }
+            this.onSubmit(this.input, this.type, this.sortedBy)
+        }
+    }
+}
+</script>
+<template>
+    <section>
+        <article>
+            <input
+                type="text"
+                v-model="input"
+                @input="onChange"
+            >
+            <section>
+                <select name="type" v-model="type" @input="onChange">
+                    <option value="book">Book</option>
+                    <option value="magazine">Magazine</option>
+                </select>
+                <select name="sortedby" v-model="sortedBy" @input="onChange">
+                    <option value="new">New</option>
+                    <option value="relevant">Relevant</option>
+                </select>
+            </section>
+        </article>
+    </section>
+</template>
+<style lang="" scoped>
+    
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <script>
+import SearchBar from '../components/private/Searchs/SearchBar.vue';
+
 export default {
-    
+    components: {
+        SearchBar
+    },
+    methods: {
+        onSubmit(input, type, sortedBy) {
+            console.log(input, type, sortedBy)
+        }
+    }
 }
 </script>
 <template lang="">
     <div class="wrapper">
-        
+        <SearchBar :onSubmit="onSubmit" />
     </div>
 </template>
 <style lang="" scoped>
