c7efccf1a72a73e870a1963a3f8af941d9384219
F0rno <pablo.fornell.perinan@gmail.com>
2024-02-07 22:15:48 +0100

    feat: contact page js validation

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,25 +1,76 @@
 <script>
 export default {
+    data() {
+        return {
+            validName: true,
+            validEmail: true,
+            validSubject: true,
+            validMessage: true,
+            firstLoad: true,
+            send: false
+        }
+    },
+    methods: {
+        onChange(event) {
+            this.firstLoad = false;
+            this.send = false;
+
+            // REGEX for name, email, subject, message
+            const regexName = /^[a-zA-Z0-9]{3,20}$/
+            const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+            const rgexSubject = /^[a-zA-Z0-9]{3,20}$/
+            const regexMsg = /^[a-zA-Z0-9]{3,255}$/
+
+            // Check if the input is valid
+            if (event.target.name === 'name') {
+                this.validName = regexName.test(event.target.value);
+            } else if (event.target.name === 'email') {
+                this.validEmail = regexEmail.test(event.target.value);
+            } else if (event.target.name === 'subject') {
+                this.validSubject = rgexSubject.test(event.target.value);
+            } else if (event.target.name === 'message') {
+                this.validMessage = regexMsg.test(event.target.value);
+            }
+        },
+        validForm() {
+            if (this.firstLoad) {
+                return false;
+            }
+            return this.validName && this.validEmail && this.validSubject && this.validMessage;
+        },
+        onSubmit() {
+            if (this.validForm()) {
+                console.log('Form sent');
+                this.send = true;
+            }
+        }
+    }
 }
 </script>
 <template lang="">
     <section class="contact-container">
         <article class="contact">
             <h2>Contact</h2>
-            <form action="mailto:admin@admin.com">
+            <form action="mailto:admin@admin.com" @submit.prevent="onSubmit">
                 <label for="name">Name</label>
-                <input type="text" id="name" name="name" required>
+                <p v-if="!validName && !firstLoad">Name is not valid</p>
+                <input type="text" id="name" name="name" required @input="onChange">
 
                 <label for="email">Email</label>
-                <input type="email" id="email" name="email" required>
+                <p v-if="!validEmail && !firstLoad">Email is not valid</p>
+                <input type="email" id="email" name="email" required @input="onChange">
 
                 <label for="subject">Subject</label>
-                <input type="text" id="subject" name="subject" required>
+                <p v-if="!validSubject && !firstLoad">Subject is not valid</p>
+                <input type="text" id="subject" name="subject" required @input="onChange">
 
                 <label for="message">Message</label>
-                <textarea id="message" name="message" required></textarea>
+                <p v-if="!validMessage && !firstLoad">Message is not valid</p>
+                <textarea id="message" name="message" required @input="onChange"></textarea>
 
-                <input type="submit" value="Contact">
+                <input type="submit" value="Contact" :class="{ 'valid-submit': !validForm() }">
+
+                <span v-if="send">Form sent</span>
             </form>
         </article>
     </section>
@@ -29,6 +80,7 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;   
+        background-color: var(--light-green);
     }
 
     .contact {
@@ -57,6 +109,11 @@ export default {
             font-size: 2.75vh;
         }
 
+        & p {
+            color: var(--yellow);
+            font-size: 2vh;
+        }
+
         & input {
             border-radius: 2px;
             border: none;
@@ -82,6 +139,16 @@ export default {
             font-weight: 700;
             font-size: 2vh;
         }
+
+        & span {
+            color: var(--white);
+            font-size: 3vh;
+            text-align: center;
+        }
+    }
+
+    .valid-submit {
+        filter: grayscale(80%);
     }
 
     @media (768px <= width) {
