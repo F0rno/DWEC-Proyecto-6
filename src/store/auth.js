b032ccb5e10617ab2ba718fc2659cf50cbb82eb8
F0rno <pablo.fornell.perinan@gmail.com>
2024-02-13 09:44:49 +0100

    internal: default login info for testing

--- src/store/auth.js
+++ src/store/auth.js
@@ -4,9 +4,9 @@ import { reactive } from 'vue';
 
 export const auth = reactive({
     id:"",
-    user: "",
-    token: "",
-    loggedin: false,
+    user: "pablo",
+    token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0L2FwaS9sb2dpbiIsImlhdCI6MTcwNzc2NzAyMCwiZXhwIjoxNzA3NzcwNjIwLCJuYmYiOjE3MDc3NjcwMjAsImp0aSI6Im84dUVibkd5aG1RZnRYbk4iLCJzdWIiOiIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.OB-u41hoajB7DQf0zeU3lyro1h_oQSkS5kcTumtF4Mw",
+    loggedin: true,
     login() {
         this.loggedin = true;
     },
