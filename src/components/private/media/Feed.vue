d5ad67b0243ebc5b7032c41a4cf92497af07b792
F0rno <pablo.fornell.perinan@gmail.com>
2024-02-16 11:25:58 +0100

    fix: bad responsive design

--- src/components/private/media/Feed.vue
+++ src/components/private/media/Feed.vue
@@ -34,7 +34,7 @@ export default {
 
     .feed__books {
         display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        grid-template-columns: repeat(auto-fill, minmax(375px, 1fr));
         gap: 1rem;
         justify-items: center;
 
