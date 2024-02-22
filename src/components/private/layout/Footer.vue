31599c641afdebee5558d09ee770328f57f70314
F0rno <pablo.fornell.perinan@gmail.com>
2024-02-22 14:08:07 +0100

    feat: info to footer

--- src/components/private/layout/Footer.vue
+++ src/components/private/layout/Footer.vue
@@ -5,12 +5,17 @@ export default {
 </script>
 <template lang="">
     <footer>
-        
+        Booky © 2024
     </footer>
 </template>
 <style lang="css" scoped>
     footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         height: 3vh;
         background-color: var(--darker-green);
+        color: var(--light-green);
+        padding: 1rem;
     }
 </style>
